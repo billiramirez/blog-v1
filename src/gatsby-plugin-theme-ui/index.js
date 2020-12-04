40bc266f1607bec170080852bcc1889fc892abb0
Billi Ramirez <ramirezbilly42@outlook.com>
2020-12-03 22:17:07 -0600

    shadowing the theme

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -5,13 +5,13 @@
  */
 
 const darkBlue = `#007acc`
-const lightBlue = `#66E0FF`
+// const lightBlue = `#66E0FF`
 const blueGray = `#282c35`
 
 export default {
-    colors: {
-        text: blueGray,
-        primary: darkBlue,
-        heading: blueGray,
-    },
+  colors: {
+    text: blueGray,
+    primary: darkBlue,
+    heading: blueGray,
+  },
 }
